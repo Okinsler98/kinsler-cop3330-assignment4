952f3d77ab32289209ed9b78df30f1039e49db01
okins <okinsler98@gmail.com>
2021-07-05 17:21:26 -0400

    Pseudo Code

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -6,4 +6,7 @@
 package ucf.assignments;
 
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/ucf/assignments/ListItem.java
@@ -0,0 +1,26 @@
+package ucf.assignments;
+
+public class ListItem {
+    String name;
+    String description;
+    String dueDate;
+    Boolean status;
+
+    public void updateName(String newName){
+        //Change name to new name
+    }
+
+    public void updateDescription(String newDescription){
+        //Change description to new descirption
+    }
+
+    public void updateDueDate(String newDate){
+        //Change date to new date
+    }
+
+    public String toString(){
+        //Return name, description, and due date
+        return "String";
+    }
+}
+
